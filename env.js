496fb1486bc5746eb21d5eef72337509f1834c29
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2023-03-17 22:30:59 +0000

    Update env.js

--- env.js
+++ env.js
@@ -13,7 +13,7 @@ const sharedEnv = {
 const TESTNET = {
     ...sharedEnv,
     APP_NAME: 'Chikoscan',
-    NETWORK_HOST: 'http://95.179.194.226:3000/?rpc=ws%3A%2F%2F70.34.216.42%3A9944#/explorer',
+    NETWORK_HOST: 'http://70.34.216.42:9944',
     NETWORK_CHAIN_ID:
       '57c70f117093411b18a5e148cc655d25509bef949acb3c5ca0288dd8806bdce9',
     NETWORK_EVM_RPC: 'http://70.34.216.42:9933',
@@ -21,7 +21,7 @@ const TESTNET = {
     NETWORK_EVM_CHAIN_ID: 42,
     HYPERION_ENDPOINT: 'http://70.34.216.42:9944',
     NETWORK_EXPLORER: 'http://95.179.194.226:3000/?rpc=ws%3A%2F%2F70.34.216.42%3A9944#/explorer',
-    TELOS_API_ENDPOINT: 'http://70.34.216.42:9944', //for local instance of api
+    TELOS_API_ENDPOINT: 'http://70.34.216.42:9933', //for local instance of api
     VERIFIED_CONTRACTS_BUCKET: 'verified-evm-contracts-testnet',
     STAKED_TLOS_CONTRACT_ADDRESS: '0xa9991E4daA44922D00a78B6D986cDf628d46C4DD',
     TELOS_ESCROW_CONTRACT_ADDRESS: '0x7E9cF9fBc881652B05BB8F26298fFAB538163b6f',
@@ -30,7 +30,7 @@ const TESTNET = {
 const MAINNET = {
     ...sharedEnv,
     APP_NAME: 'Chikoscan',
-    NETWORK_HOST: 'http://95.179.194.226:3000/?rpc=ws%3A%2F%2F70.34.216.42%3A9944#/explorer',
+    NETWORK_HOST: 'http://70.34.216.42:9944',
     NETWORK_CHAIN_ID:
       '57c70f117093411b18a5e148cc655d25509bef949acb3c5ca0288dd8806bdce9',
     NETWORK_EVM_RPC: 'http://70.34.216.42:9933',
