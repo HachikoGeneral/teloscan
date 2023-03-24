5334e96a8ecced7ad64c94e8d3d0c54951cc3b5a
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2023-03-24 22:36:17 +0000

    Update env.js

--- env.js
+++ env.js
@@ -7,19 +7,19 @@ The environment variables ares set based on the single variable `MAINNET=true`
 const sharedEnv = {
     NETWORK_PROTOCOL: 'http',
     NETWORK_PORT: 9933,
-    NETWORK_EVM_CONTRACT: 'eosio.evm',
+    NETWORK_EVM_CONTRACT: 'chikochain.evm',
 };
 
 const TESTNET = {
     ...sharedEnv,
     APP_NAME: 'Chikoscan',
-    NETWORK_HOST: 'http://70.34.216.42:9944',
+    NETWORK_HOST: 'http://70.34.216.42:9933',
     NETWORK_CHAIN_ID:
       '57c70f117093411b18a5e148cc655d25509bef949acb3c5ca0288dd8806bdce9',
     NETWORK_EVM_RPC: 'http://70.34.216.42:9933',
-    NETWORK_EVM_ENDPOINT: 'http://70.34.216.42:9944',
+    NETWORK_EVM_ENDPOINT: 'http://70.34.216.42:9933',
     NETWORK_EVM_CHAIN_ID: 42,
-    HYPERION_ENDPOINT: 'http://70.34.216.42:9944',
+    HYPERION_ENDPOINT: 'http://70.34.216.42:9933',
     NETWORK_EXPLORER: 'http://95.179.194.226:3000/?rpc=ws%3A%2F%2F70.34.216.42%3A9944#/explorer',
     TELOS_API_ENDPOINT: 'http://70.34.216.42:9933', //for local instance of api
     VERIFIED_CONTRACTS_BUCKET: 'verified-evm-contracts-testnet',
@@ -30,15 +30,15 @@ const TESTNET = {
 const MAINNET = {
     ...sharedEnv,
     APP_NAME: 'Chikoscan',
-    NETWORK_HOST: 'http://70.34.216.42:9944',
+    NETWORK_HOST: 'http://70.34.216.42:9933',
     NETWORK_CHAIN_ID:
       '57c70f117093411b18a5e148cc655d25509bef949acb3c5ca0288dd8806bdce9',
     NETWORK_EVM_RPC: 'http://70.34.216.42:9933',
-    NETWORK_EVM_ENDPOINT: 'http://70.34.216.42:9944',
+    NETWORK_EVM_ENDPOINT: 'http://70.34.216.42:9933',
     NETWORK_EVM_CHAIN_ID: 42,
-    HYPERION_ENDPOINT: 'http://70.34.216.42:9944',
+    HYPERION_ENDPOINT: 'http://70.34.216.42:9933',
     NETWORK_EXPLORER: 'http://95.179.194.226:3000/?rpc=ws%3A%2F%2F70.34.216.42%3A9944#/explorer',
-    TELOS_API_ENDPOINT: 'http://70.34.216.42:9944', //for local instance of api
+    TELOS_API_ENDPOINT: 'http://70.34.216.42:9933', //for local instance of api
     VERIFIED_CONTRACTS_BUCKET: 'verified-evm-contracts',
     STAKED_TLOS_CONTRACT_ADDRESS: '0xB4B01216a5Bc8F1C8A33CD990A1239030E60C905',
     TELOS_ESCROW_CONTRACT_ADDRESS: '0x95F5713A1422Aa3FBD3DCB8D553945C128ee3855',
